9148c4040f5a5573a827552c7c55879900f0a670
Mithibe Irungu <Kenmithibe@gmail.com>
2025-03-04 00:43:45 +0300

    Live update available

--- Nguo-frontend/src/App.vue
+++ Nguo-frontend/src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
+import { ref } from "vue";
 import AddNguo from './components/AddNguo.vue';
 import Nguolist from './components/Nguolist.vue';
 
+const nguoListRef = ref(null); // Reference to NguoList component
+
+const refreshList = () => {
+  if (nguoListRef.value) {
+    nguoListRef.value.fetchNguo(); // Call fetchNguo in NguoList.vue
+  }
+};
 </script>
 
 <template>
   <div>
     <h1>Nguo Management</h1>
-    <AddNguo/>
-    <Nguolist/>
+    <AddNguo @itemAdded="refreshList"/>
+    <Nguolist ref="nguoListRef"/>
   </div>
 </template>
 
